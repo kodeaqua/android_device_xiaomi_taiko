9679cc0ab60a256e70926769f54f5861f765a1b0
YagizErdemir06 <erdemiryagiz@proton.me>
2025-07-03 17:46:02 +0300

    emerald: Inherit `generic_ramdisk` product configuration
    Change-Id: I542088bc7125a02b1bfd4ee6d5f0673be91e9849
    Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

--- device.mk
+++ device.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit generic_ramdisk product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 DEVICE_PATH := device/xiaomi/emerald
 KERNEL_PATH := $(DEVICE_PATH)-kernel
 
