b513a65834b13d40928fd0cd393eee5f1976f680
YagizErdemir06 <erdemiryagiz@proton.me>
2025-07-03 19:00:07 +0300

    emerald: Define partition layout, sizes and types
    Change-Id: I7e6833fbf7aa92b151f569633ccfe059ed0d4a4d
    Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

--- device.mk
+++ device.mk
@@ -15,6 +15,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 DEVICE_PATH := device/xiaomi/emerald
 KERNEL_PATH := $(DEVICE_PATH)-kernel
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
