05c35b7b343d473bf63a4d7f13fc1f3768d38299
YagizErdemir06 <erdemiryagiz@proton.me>
2025-07-07 01:40:16 +0300

    emerald: fixup! to dt2w path
    Change-Id: I69847ce559cbdd254431f9df819fd0d39343f7dc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := mt6789
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
