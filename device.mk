e6a35212868ed181ebd8b331e5fd09d022d1dd63
YagizErdemir06 <erdemiryagiz@proton.me>
2025-07-09 01:16:29 +0300

    emerald: Configure Keymint stack
    Change-Id: I5d971cafaaac9a8715dc5c66d3974de0b7ab1109
    Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

--- device.mk
+++ device.mk
@@ -126,6 +126,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# Keymint
+PRODUCT_PACKAGES += \
+    android.hardware.security.keymint-V1-ndk_platform.vendor \
+    android.hardware.security.secureclock-V1-ndk_platform.vendor \
+    android.hardware.security.sharedsecret-V1-ndk_platform.vendor \
+    android.hardware.security.rkp-V1-ndk.vendor \
+    lib_android_keymaster_keymint_utils.vendor \
+    libkeymint.vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.hardware_keystore.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.1.vendor:64 \
