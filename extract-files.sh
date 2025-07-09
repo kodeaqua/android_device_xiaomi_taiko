e2328a9613132a4f920dd3221a84317cfc3a8cd6
SamarV-121 <samarvispute121@pm.me>
2025-07-09 21:35:31 +0300

    emerald: Use libutils-v32 for libmtkcam_stdutils.so
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Change-Id: I8e4466f68660ea5dca23a51e624e46561cd73d10
    Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,9 @@ function blob_fixup {
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             "${PATCHELF}" --replace-needed "libsensorndkbridge.so" "android.hardware.sensors@1.0-convert-shared.so" "${2}"
             ;;
+        vendor/lib*/libmtkcam_stdutils.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
+            ;;
     esac
 }
 
