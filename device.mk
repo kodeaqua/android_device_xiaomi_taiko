d05989c9e1710dc0c904397df6b6d74f60a5d3f8
Signed-off-by: bengris32 <bengris32@protonmail.ch>
2025-07-12 21:03:09 +0300

    emerald: Switch to common MediaTek USB service
    Change-Id: Iba9a20ed667d18ab2f99e3bf5a6f7e353f392484
    Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

--- device.mk
+++ device.mk
@@ -366,14 +366,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0.vendor:64 \
-    android.hardware.usb@1.1.vendor:64 \
-    android.hardware.usb@1.2.vendor:64 \
-    android.hardware.usb@1.3.vendor:64 \
-    android.hardware.usb.gadget@1.0.vendor:64 \
-    android.hardware.usb.gadget@1.1.vendor:64
+$(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
 
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
 
 # Vibrator
 PRODUCT_PACKAGES += \
